12c23155267c62633b2ad2b0b97a01bc4caba728
Stan Matrosov <matrosovdream@gmail.com>
2025-11-17 08:49:22 +0100

    EntryUpdateTypeSeeder, initial version

---
+++ database/seeders/Frm/EntryUpdateTypeSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders\Frm;
+
+use Illuminate\Database\Seeder; 
+use App\Models\FrmEntryUpdateType;
+
+class EntryUpdateTypeSeeder extends Seeder {
+
+    public function run() {
+
+        $items = $this->getItems();
+
+        foreach ($items as $item) {
+            FrmEntryUpdateType::firstOrCreate(
+                [ 'code' => $item['code'] ], 
+                $item);
+        }
+
+    }
+
+    protected function getItems() {
+        return [
+            ['id' => 1, 'code' => 'create', 'title' => 'Create'],
+            ['id' => 2, 'code' => 'update', 'title' => 'Update'],
+            ['id' => 3, 'code' => 'delete', 'title' => 'Delete'],
+        ];
+    }
+
+}
