1c320732861cc0c0e5a20fa0165113c5841e05ff
Stan Matrosov <matrosovdream@gmail.com>
2025-11-17 08:46:41 +0100

    FrmEntryHistory Model, initial version

---
+++ app/Models/FrmEntryHistory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class FrmEntryHistory extends Model
+{
+    
+    protected $fillable = [
+        'entry_id',
+        'site_id',
+        'field_id',
+        'update_type_id',
+        'value'
+    ];
+
+    protected $table = 'frm_entry_history';
+
+    public function site() {
+        return $this->belongsTo(Site::class);
+    }
+
+    public function field() {
+        return $this->belongsTo(FrmField::class);
+    }
+
+}
