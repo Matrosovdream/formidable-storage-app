45bc43cf62afb984e9a06deb0acf33f48d31e178
Stan Matrosov <matrosovdream@gmail.com>
2025-11-19 14:07:02 +0100

    RestTokenMiddleware, injected SiteTokenRepo

--- app/Http/Middleware/Rest/RestTokenMiddleware.php
+++ app/Http/Middleware/Rest/RestTokenMiddleware.php
@@ -2,16 +2,28 @@
 
 namespace App\Http\Middleware\Rest;
 
+use App\Services\Site\SiteTokenService;
 use Closure;
 
-class RestTokenMiddleware
+class RestTokenMiddleware extends AbstractMiddleware
 {
+
+    public function __construct(
+        protected SiteTokenService $tokenService
+    )
+    {
+
+    }
+
     public function handle($request, Closure $next)
     {
         $token = $request->bearerToken();
 
-        if (!$token || $token !== '123') {
-            return response()->json(['message' => 'Unauthorized'], 401);
+        if (
+            !$token ||
+            !$this->tokenService->validateToken( $token )
+        ) {
+            return $this->returnUnauthorizedResponse();
         }
 
         return $next($request);
