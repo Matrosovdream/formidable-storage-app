4753508a59b1bd47282f8fbfbfb9d84ab7708395
Stan Matrosov <matrosovdream@gmail.com>
2025-12-01 14:15:11 +0700

    FrmFieldsActions, extend abstract class

---
+++ app/Http/Actions/Rest/FrmFieldsActions.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Actions\Rest;
+
+use App\Services\Frm\FrmFieldService;
+
+class FrmFieldsActions extends ActionsRestAbstract
+{
+
+    protected $fieldService;
+
+    public function __construct()
+    {
+        $this->fieldService = new FrmFieldService();
+    }
+
+    public function updateAll( $request )
+    {
+
+        $data = [
+            'site_id' => 1,
+            'fields'  => $request['fields'] ?? [],
+        ];
+
+        $res = $this->fieldService->updateFieldsAll( $data );
+
+        if ( $res ) {
+            return $this->returnSuccess( 'Formidable fields updated successfully.' );
+        } else {
+            return $this->returnError( 'Failed to update Formidable fields.' );
+        }
+    }
+
+}
