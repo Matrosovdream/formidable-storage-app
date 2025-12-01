c0a043be3a0b8ba58d951a6235fde6384e69bf32
Stan Matrosov <matrosovdream@gmail.com>
2025-12-01 14:15:48 +0700

    FrmFieldService, some cleanup

--- app/Services/Frm/FrmFieldService.php
+++ app/Services/Frm/FrmFieldService.php
@@ -2,11 +2,45 @@
 
 namespace App\Services\Frm;
 
+use App\Repositories\Frm\FrmFieldRepo;
+
 class FrmFieldService {
+
+    protected $fieldRepo;
+
+    public function __construct() {
+        $this->fieldRepo = new FrmFieldRepo();
+    }
 
     public function updateFieldsAll( $request ) {
         
-        return false;
+        $site_id = $request['site_id'] ?? null;
+        $fields = $request['fields'] ?? [];
+
+        foreach ( $fields as $fieldData ) {
+            $field_id = $fieldData['field_id'] ?? null;
+            $type     = $fieldData['type'] ?? null;
+            $key      = $fieldData['field_key'] ?? null;
+            $label    = $fieldData['label'] ?? null;
+
+            if ( $site_id && $field_id ) {
+                
+                $this->fieldRepo->model->updateOrCreate(
+                    [
+                        'site_id'  => $site_id,
+                        'field_id' => $field_id,
+                    ],
+                    [
+                        'key'    => $key,
+                        'type'   => $type,
+                        'label'  => $label
+                    ]
+                );
+            }
+        }
+
+        return true;
+
     }
 
 }
