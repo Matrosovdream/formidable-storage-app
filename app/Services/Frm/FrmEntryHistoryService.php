74e7d2852adef4383147093bf0eadab7cdaebcc8
Stan Matrosov <matrosovdream@gmail.com>
2025-12-03 09:38:15 +0700

    FrmEntryHistoryService, injected new field change_date

--- app/Services/Frm/FrmEntryHistoryService.php
+++ app/Services/Frm/FrmEntryHistoryService.php
@@ -6,22 +6,26 @@ use Illuminate\Support\Facades\DB;
 use Throwable;
 use App\Repositories\Frm\FrmEntryHistoryRepo;
 use App\Repositories\Frm\FrmEntryUpdateTypeRepo;
+use App\Repositories\Frm\FrmFieldRepo;
 
 class FrmEntryHistoryService {
 
     protected $historyRepo;
     protected $updateTypeRepo;
+    protected $fieldRepo;
 
     public function __construct() {
         $this->historyRepo = new FrmEntryHistoryRepo();
         $this->updateTypeRepo = new FrmEntryUpdateTypeRepo();
+        $this->fieldRepo = new FrmFieldRepo();
     }
 
     public function getEntryHistory(int $entry_id, array $site)
     {
 
         $types = $this->getUpdateTypes();
-        
+        $fieldsMap = $this->getFieldsMap($site);
+
         // Get by entry_id and site_id
         $site_id = $site['id'];
         $history = $this->historyRepo->model
@@ -36,13 +40,40 @@ class FrmEntryHistoryService {
             $itemsRaw = $history->toArray();
             $items = [];
             foreach ( $itemsRaw as $item ) {
+
+                // Update type
                 $item['update_type'] = $types[ $item['update_type_id'] ] ?? 'unknown';
                 unset( $item['update_type_id'] );
+
+                // Field info
+                $item['field'] = $fieldsMap[ $item['field_id'] ] ?? null;
+
                 $items[] = $item;
             }
 
             return $items;
         }
+
+    }
+
+    public function getFieldsMap(array $site): array
+    {
+
+        $site_id = $site['id'];
+        $fieldsRaw = $this->fieldRepo->model
+            ->where('site_id', $site_id)
+            ->get();
+
+        $fields = [];
+        foreach ( $fieldsRaw as $field ) {
+            $fields[ $field->field_id ] = [
+                'key'   => $field->key,
+                'type'  => $field->type,
+                'label' => $field->label
+            ];
+        }
+
+        return $fields;
 
     }
 
@@ -71,6 +102,7 @@ class FrmEntryHistoryService {
                 'field_id'       => $item['field_id'],
                 'update_type_id' => 2, // Updated
                 'value'          => $item['value'],
+                'change_date'    => $item['change_date'],
             ];
         }
     
@@ -82,6 +114,7 @@ class FrmEntryHistoryService {
                 'field_id'       => $item['field_id'],
                 'update_type_id' => 1, // Created
                 'value'          => $item['value'],
+                'change_date'    => $item['change_date']
             ];
         }
     
@@ -99,6 +132,7 @@ class FrmEntryHistoryService {
                     [
                         'site_id' => $entryData['site_id'],
                         'value' => $entryData['value'],
+                        'change_date' => $entryData['change_date']
                     ]
                 );
             }
