3e8abeaa0c1ce5b917092c6cb5d0474e94ff94d1
Stan Matrosov <matrosovdream@gmail.com>
2025-12-09 14:57:40 +0700

    SiteRepo, new methods deleteSite/addSite

--- app/Repositories/Site/SiteRepo.php
+++ app/Repositories/Site/SiteRepo.php
@@ -26,6 +26,62 @@ class SiteRepo extends AbstractRepo
         return $this->getById( $token['site_id'] );
     }
 
+    public function getSiteToken( int $site_id )
+    {
+        $tokenRepo = new SiteTokenRepo();
+        
+        $token = $tokenRepo->model
+            ->where('site_id', $site_id)
+            ->first();
+
+        return $token ? $token->token : null;
+    }
+
+    public function addSiteToken( int $site_id )
+    {
+
+        $tokenRepo = new SiteTokenRepo();
+
+        // Remove existing token if any
+        $tokenRepo->model
+            ->where('site_id', $site_id)
+            ->delete();
+        
+        $token = bin2hex(random_bytes(16));
+
+        $tokenData = [
+            'site_id' => $site_id,
+            'token' => $token
+        ];
+
+        $tokenRepo->create( $tokenData );
+
+        return $token;
+    }
+
+    public function addSite( array $data )
+    {
+        $site = $this->create( $data );
+
+        // Create token
+        $this->addSiteToken( $site['id'] );
+
+        return $this->getById( $site['id'] );
+    }
+
+    public function deleteSite( int $site_id )
+    {
+        $tokenRepo = new SiteTokenRepo();
+
+        // Delete token
+        $tokenRepo->model
+            ->where('site_id', $site_id)
+            ->delete();
+
+        // Delete site
+        return $this->delete( $site_id );
+    }
+
     public function mapItem($item)  
     {
         if (empty($item)) {
@@ -36,6 +92,7 @@ class SiteRepo extends AbstractRepo
             'id' => $item->id,
             'name' => $item->name,
             'url' => $item->url,
+            'token' => $this->getSiteToken( $item->id ),
             'Model' => $item
         ];
         return $res;
