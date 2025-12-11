1e3c0b4bbd012c84647897b5d3458521a466c10d
Stan Matrosov <matrosovdream@gmail.com>
2025-12-11 22:17:59 +0700

    FrmEmailsLogActions, initial version

---
+++ app/Http/Actions/Rest/Frm/FrmEmailsLogActions.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Actions\Rest\Frm;
+
+use App\Http\Actions\Rest\ActionsRestAbstract;
+use App\Services\Frm\FrmEmailLogService;
+use App\Jobs\Frm\UpdateEmailsLogJob;
+
+class FrmEmailsLogActions extends ActionsRestAbstract
+{
+
+    protected $logService;
+
+    public function __construct()
+    {
+
+        parent::__construct();
+
+        $this->logService = new FrmEmailLogService();
+    }
+
+    public function updateAll( $request )
+    {
+
+        $data = $this->prepareRequestData($request);
+
+        /*
+        $res = $this->logService->updateDataMultiple(
+            $data['data'],
+            $data['site']
+        );
+        */
+
+        // Dispatch job to queue
+        UpdateEmailsLogJob::dispatch(
+            $data['data'],
+            $data['site']
+        );
+
+        return $this->returnSuccess('Formidable fields queued for update.');
+    }
+
+}
