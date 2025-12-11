17c13a99c67a21fe5852901afae58e8e1156518d
Stan Matrosov <matrosovdream@gmail.com>
2025-12-11 22:17:39 +0700

    FrmEmailsLogController, initial version

---
+++ app/Http/Controllers/Rest/v1/FrmEmailsLogController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers\Rest\v1;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Actions\Rest\Frm\FrmEmailsLogActions;
+
+class FrmEmailsLogController extends Controller
+{
+
+    public function __construct(
+        protected FrmEmailsLogActions $actions,
+    ) {
+
+    }
+
+    public function updateAll( Request $request )
+    {
+        return $this->actions->updateAll( $request );
+    }
+
+}
