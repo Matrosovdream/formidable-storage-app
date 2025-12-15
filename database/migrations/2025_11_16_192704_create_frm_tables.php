067ece025b221084215a288f5de8bd0d5be40b29
Stan Matrosov <matrosovdream@gmail.com>
2025-12-15 21:04:09 +0700

    New migration for Email logs, removed from the old file

--- database/migrations/2025_11_16_192704_create_frm_tables.php
+++ database/migrations/2025_11_16_192704_create_frm_tables.php
@@ -44,32 +44,6 @@ return new class extends Migration
             $table->timestamps();
         });
 
-        Schema::create('frm_emails_log', function (Blueprint $table) {
-            $table->id();
-            $table->integer('entry_id')->nullable();
-            $table->foreignId('site_id')->on('sites');
-            $table->integer('form_id')->nullable();
-            $table->string('subject');
-            $table->string('message_id')->nullable();
-            $table->string('email_from')->nullable();
-            $table->string('email_to')->nullable();
-            $table->text('people')->nullable();
-            $table->text('headers')->nullable();
-            $table->text('error_text')->nullable();
-            $table->longText('content_plain');
-            $table->longText('content_html');
-            $table->tinyInteger('status')->default(0);
-            $table->timestamp('date_sent')->nullable();
-            $table->string('mailer')->nullable();
-            $table->tinyInteger('attachments')->default(0);
-            $table->string('initiator_name')->nullable();
-            $table->text('initiator_file')->nullable();
-            $table->integer('original_log_id')->nullable();
-            $table->timestamps();
-
-            $table->unique(['site_id', 'message_id'], 'frm_emails_log_unique');
-        });
-
     }
 
     /**
@@ -80,7 +54,5 @@ return new class extends Migration
         Schema::dropIfExists('frm_entry_history');
         Schema::dropIfExists('frm_fields');
         Schema::dropIfExists('frm_entry_update_types');
-        Schema::dropIfExists('frm_emails_log');
-        
     }
 };
---
+++ database/migrations/2025_12_15_135311_create_frm_email_logs_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('frm_emails_log', function (Blueprint $table) {
+            $table->id();
+            $table->integer('entry_id')->nullable();
+            $table->foreignId('site_id')->on('sites');
+            $table->integer('form_id')->nullable();
+            $table->string('subject');
+            $table->string('message_id')->nullable();
+            $table->string('email_from')->nullable();
+            $table->string('email_to')->nullable();
+            $table->text('people')->nullable();
+            $table->text('headers')->nullable();
+            $table->text('error_text')->nullable();
+            $table->longText('content_plain');
+            $table->longText('content_html');
+            $table->tinyInteger('status')->default(0);
+            $table->timestamp('date_sent')->nullable();
+            $table->string('mailer')->nullable();
+            $table->tinyInteger('attachments')->default(0);
+            $table->string('initiator_name')->nullable();
+            $table->text('initiator_file')->nullable();
+            $table->integer('original_log_id')->nullable();
+            $table->timestamps();
+
+            $table->unique(['site_id', 'message_id'], 'frm_emails_log_unique');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('frm_emails_log');
+    }
+};
